ca6bd961b1b6917a779ae9f0fd39295ff5e39d95
mariotl13 <mariotl1310@gmail.com>
2019-04-22 13:40:02 +0200

    Update app.js

--- server/app.js
+++ server/app.js
@@ -5,7 +5,7 @@ if (process.env.NODE_ENV === 'production') {
   // Express serve up index.html file if it doesn't recognize route
   const path = require('path');
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'server', 'app.js'));
+    res.sendFile(path.resolve(__dirname, 'index.html'));
   });
 }
 
