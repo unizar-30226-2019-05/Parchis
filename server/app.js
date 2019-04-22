0d695272d5f72f0b3eb6f17be0861e89ac298cd2
mariotl13 <mariotl1310@gmail.com>
2019-04-22 18:39:04 +0200

    Nada

--- server/app.js
+++ server/app.js
@@ -1,16 +1,3 @@
-/*
-if (process.env.NODE_ENV === 'production') {
-  // Exprees will serve up production assets
-  app.use(express.static(''));
-
-  // Express serve up index.html file if it doesn't recognize route
-  const path = require('path');
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'index.html'));
-  });
-}
-*/
-
 // BASE DE DATOS
 const express = require('express')
 const app = express()
